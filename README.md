b08accfe9c915df3863642abc35aff076a3fcd81
git config --global user.name rodneaa <rodneaareese@gmail.com>
2023-08-21 16:44:06 -0400

    Updated files , WeatherPy complete. VacationPy in process

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-api-challenge
 Module 6 Homework
+
+#used for field definitions
+https://openweathermap.org/current#fields_json
---
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "72e10dbab5d296ed163f7b64e8b29a64"
+
+# Geoapify API Key
+geoapify_key = "51dd70c70e0d4d94b86f03e10601a549"
